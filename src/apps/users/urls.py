4c81f4f44438e4e4165ceb9efada6e32afb142e1
AlekseiChirkov <tektonikboy98@gmail.com>
2021-12-21 16:11:04 +0300

    Created profile page

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.UserSignUpFormView.as_view(), name='signup'),
-    path('login/', views.UserLoginFormView.as_view(), name='login')
+    path('login/', views.UserLoginFormView.as_view(), name='login'),
+    path('profile/<int:pk>', views.UserProfileView.as_view(), name='profile'),
 ]
