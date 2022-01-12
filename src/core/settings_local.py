992ca1e5fed8148652e58d72228c11cfc0af4dd6
Scareface <tektonikboy98@gmail.com>
2022-01-12 09:57:05 +0300

    Changed envs

--- src/core/settings_local.py
+++ src/core/settings_local.py
@@ -1,7 +1,5 @@
 # import sentry_sdk
 # from sentry_sdk.integrations.django import DjangoIntegration
-import os
-
 from decouple import config
 
 
@@ -29,10 +27,10 @@ from decouple import config
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = config('DEBUG', default=True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,10 +40,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
